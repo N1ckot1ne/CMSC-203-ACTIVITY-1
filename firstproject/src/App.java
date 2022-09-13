2f850dce0a43e8528d155555a77ab7126c5f9040
Venancio, Nicko A <106007314+N1ckot1ne@users.noreply.github.com>
2022-09-13 19:26:53 +0800

    Add files via upload

---
+++ firstproject/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String [] args) {
+        
+        String firstname = "Nicko";
+        String lastname = "Venancio";
+        int age = 20;
+        float gpa = 1.52f;
+        String bloodtype = "unknown";
+        
+        System.out.println("Hi, My name is " + firstname + lastname);
+        System.out.println("I am " + age + "years old.");
+        System.out.println("My GPA is " + gpa);
+        System.out.println("my bloodtype is " + bloodtype);
+        
+    }
+}
